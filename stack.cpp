dfca65cc046cdfa74d7d687f7452f2a681d8da61
Enzolpw <enzolp2@hotmail.com>
2023-10-18 15:11:53 -0400

    Header Stack File

--- stack.cpp
+++ stack.cpp
@@ -1,38 +1,60 @@
-namespace structures {
+#include "stack.h"
 
 template<typename T>
-//! CLASSE PILHA
-class ArrayStack {
- public:
-    //! construtor simples
-    ArrayStack();
-    //! construtor com parametro tamanho
-    explicit ArrayStack(std::size_t max);
-    //! destrutor
-    ~ArrayStack();
-    //! metodo empilha
-    void push(const T& data);
-    //! metodo desempilha
-    T pop();
-    //! metodo retorna o topo
-    T& top();
-    //! metodo limpa pilha
-    void clear();
-    //! metodo retorna tamanho
-    std::size_t size();
-    //! metodo retorna capacidade maxima
-    std::size_t max_size();
-    //! verifica se esta vazia
-    bool empty();
-    //! verifica se esta cheia
-    bool full();
+structures::ArrayStack<T>::ArrayStack() {
+   ArrayStack(DEFAULT_SIZE);
+}
 
- private:
-    T* contents;
-    int top_;
-    std::size_t max_size_;
+template<typename T>
+structures::ArrayStack<T>::ArrayStack(std::size_t max) {
+    // Implementação do construtor com parâmetro
+    max_size_ = max;
+    contents = new T[max_size_];
+    top_ = -1;
+}
 
-    static const auto DEFAULT_SIZE = 10u;
-};
+template<typename T>
+structures::ArrayStack<T>::~ArrayStack() {
+   // Implementação do destrutor
+   delete [] contents;
+}
 
-}  // namespace structures
+template<typename T>
+void structures::ArrayStack<T>::push(const T& data) {
+    // Implementação do método push
+}
+
+template<typename T>
+T structures::ArrayStack<T>::pop() {
+    // Implementação do método pop
+}
+
+template<typename T>
+T& structures::ArrayStack<T>::top() {
+    // Implementação do método top
+}
+
+template<typename T>
+void structures::ArrayStack<T>::clear() {
+    // Implementação do método top
+}
+
+template<typename T>
+std::size_t structures::ArrayStack<T>::size() {
+    // Implementação do método size
+}
+
+template<typename T>
+std::size_t structures::ArrayStack<T>::max_size() {
+    // Implementação do método max_size
+}
+
+template<typename T>
+bool structures::ArrayStack<T>::empty() {
+    // Implementação do método empty
+}
+
+template<typename T>
+bool structures::ArrayStack<T>::full() {
+    // Implementação do método top
+}
