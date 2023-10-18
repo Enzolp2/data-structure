b8f92ad1e3831881be14e6509668b398b5acae0f
Enzolpw <enzolp2@hotmail.com>
2023-10-18 14:35:29 -0400

    Estrutura de arquivos

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+    char xmlfilename[100];
+
+    std::cin >> xmlfilename;  // entrada
+    
+    
+
+    std::cout << xmlfilename << std::endl;  // esta linha deve ser removida
+
+    return 0;
+}
+
